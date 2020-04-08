0d4b90392e363b24f2a67f443dac8bc6ff264fb1
aahan_5833 <aahancharak@gmail.com>
2020-04-09 00:34:12 +0530

    Added the build controls layout

---
+++ burger/src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Classes from './BuildControl.module.css';
+const buildControl = (props) => (
+  <div className={Classes.BuildControl}>
+    <div className={Classes.Label}>{props.label}</div>
+    <button className={Classes.More}>More</button>
+    <button className={Classes.Less}>Less</button>
+  </div>
+);
+
+export default buildControl;
