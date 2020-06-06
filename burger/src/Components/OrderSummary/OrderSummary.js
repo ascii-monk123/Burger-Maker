6f807d54e69c1c1c1137166f53c3344d270f320b
aahan_5833 <aahancharak@gmail.com>
2020-06-06 23:34:44 +0530

    Added some optimizations to the modal and ordersummary.js file

--- burger/src/Components/OrderSummary/OrderSummary.js
+++ burger/src/Components/OrderSummary/OrderSummary.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../HOC/Auxillary';
 import Button from '../UI/Button/Button';
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('order summary will update');
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Delicious Burger with following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price :${props.totalPrice.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout</p>
-      <Button buttonType={'Danger'} clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button buttonType={'Success'} clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Delicious Burger with following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price :${this.props.totalPrice.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout</p>
+        <Button buttonType={'Danger'} clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button buttonType={'Success'} clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
 
 export default OrderSummary;
--- burger/src/Components/UI/Modal.js
+++ burger/src/Components/UI/Modal.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Classes from './Modal.module.css';
 import Aux from '../../HOC/Auxillary';
 import Backdrop from '../UI/Backdrop/Backdrop';
-const Modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={Classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Shoud modal update');
+    //here we made sure that we dont unnecessarily update the ordersummary and shall only update it when the modal is to be shown and order summary will not be updated all the times
+    //Here the order summary does not rerender cause the modal is the wrapping element which is controlling the behavior and isn't letting it update without any reason.
+    return nextProps.show !== this.props.show;
+  }
+  componentWillUpdate() {
+    //this is not required here
+    console.log('Modal will update');
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={Classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 export default Modal;
