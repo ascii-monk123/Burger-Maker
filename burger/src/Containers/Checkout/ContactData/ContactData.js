b88f8d1df30bb1d18944d408d7feeeb471aa8074
aahan_5833 <aahancharak@gmail.com>
2020-08-23 23:15:15 +0530

    Added the feedback validation

--- burger/src/Containers/Checkout/ContactData/ContactData.js
+++ burger/src/Containers/Checkout/ContactData/ContactData.js
@@ -14,6 +14,10 @@ class ContactData extends Component {
           placeholder: 'Your Name',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       street: {
         elementType: 'input',
@@ -22,6 +26,10 @@ class ContactData extends Component {
           placeholder: 'Street',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       zipCode: {
         elementType: 'input',
@@ -30,6 +38,12 @@ class ContactData extends Component {
           placeholder: 'zip-code',
         },
         value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 7,
+        },
+        valid: false,
       },
       country: {
         elementType: 'input',
@@ -38,6 +52,10 @@ class ContactData extends Component {
           placeholder: 'Country',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       email: {
         elementType: 'input',
@@ -46,6 +64,10 @@ class ContactData extends Component {
           placeholder: 'Your email',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       deliveryMethod: {
         elementType: 'select',
@@ -60,6 +82,20 @@ class ContactData extends Component {
     },
 
     loading: false,
+  };
+
+  checkValidity = (value, rules) => {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
   };
 
   orderHandler = (event) => {
@@ -93,6 +129,10 @@ class ContactData extends Component {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     this.setState({
       orderForm: updatedOrderForm,
