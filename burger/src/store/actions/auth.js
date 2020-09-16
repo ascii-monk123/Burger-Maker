8fa56df25eedf0b4c1603ba34b0b93699d82b192
Aahan Singh Charak <aahancharak@gmail.com>
2020-09-16 19:56:10 +0530

    Added signup using firebase to the project

--- burger/src/store/actions/auth.js
+++ burger/src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 export const authStart = () => {
   return {
@@ -22,5 +23,23 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart);
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCPePYryAezEAba3HDtXLDe6Pp0isS1qe0",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(success(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
